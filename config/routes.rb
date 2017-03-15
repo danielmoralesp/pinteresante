f920a187b3beb364ab27d7efe2d04a54b4f18206
danielmoralesp <daniel_moralesp@hotmail.com>
2017-03-15 13:44:17 -0500

    implementa la funcionalidad de listar y crear pines

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  root to: "home#index"
-
+  root 'pins#index'
+  resources :pins
   devise_for :users
 
   # The priority is based upon order of creation: first created -> highest priority.
